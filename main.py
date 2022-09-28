468082a3126be1e05d4454a539844c27d54f12f3
FernandaMascarenhas <fer.mascarenhass@gmail.com>
2022-09-27 20:47:32 -0400

    signup: verifica se email ja existe, cadastra user

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 import pandas as pd
 from fastapi import FastAPI
 
@@ -7,9 +8,17 @@ app = FastAPI()
 def home():
     return {"ta on"}
 
-@app.get("/sign-up")
-def cadastrar():
-    return {"cadastrando"}
+@app.post("/sign-up", status_code=status.HTTP_201_CREATED, response_model=User)
+def sign_up_user(user: User, session: Session = Depends(get_db)):
+    # Verifica se já existe um usuário com esse email
+    registered_user = Repository(session).get_user_by_email(user.email)
+    if registered_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="E-mail already registered!")
+
+    # Criar usuário
+    user.password = security.get_password_hash(user.password)    #hash para senha
+    created_user = Repository(session).create(user)
+    return created_user
 
 @app.get("/login")
 def login():
