1c594289e86cd2edbdf3f311950b5d17592f7268
FernandaMascarenhas <fer.mascarenhass@gmail.com>
2022-10-06 19:57:20 -0400

    CORS : determinar origem das requisições

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ import security
 import token_provider
 import utils
 import pandas as pd
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#CORS
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
