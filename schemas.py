2715f70f49aafeff754a3f9421108fdd5de6eb8d
fernandabarrigosse <110494800+fernandabarrigosse@users.noreply.github.com>
2022-10-12 15:07:06 -0400

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,45 @@
+from typing import ItemsView, List, Union, Optional
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    email: str
+    id: int
+    hashed_password: str
+    name: str
+
+    projects: List[ItemsView] = []
+
+class Project(BaseModel):
+    id: int
+    author: str
+    execution_number: int
+
+    user: str
+    
+class Execution(BaseModel):
+    id: int
+    qgd: str 
+    test_number: int
+    result_id: int
+
+    results: List[ItemsView] = []
+
+class Result (BaseModel):
+    id: int
+    graph_id: int
+    min_df: float
+    Topics: int
+    Words: int
+    Similar_Words: int
+    No_Results_total: int
+    No_QGS: int
+    No_GS_Rsts: int
+    No_total_SB: int
+    Precision: float
+    Recall: float
+    Recall_final: float
+    F1_Sts: float
+    QGS: int 
+    
+    owner: str
